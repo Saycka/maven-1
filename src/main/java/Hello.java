c6818b7700227975fae2a5cc50333c4f17399977
Andrey <a>
2018-10-15 20:35:56 +0300

    branch2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     }
 
     public static String helloAge(int age) {
+        System.out.println("branch_2");
         if (age <= 0) {
             throw new IllegalArgumentException();
         }
