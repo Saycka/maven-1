5404393884332bdab59f8ccd644fd11bdf196d50
Andrey <a>
2018-10-15 20:40:50 +0300

    branch master

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
     public static String helloAge(int age) {
 
         System.out.println("branch master");
+        System.out.println("branch master");
         if (age <= 0) {
             throw new IllegalArgumentException();
         }
